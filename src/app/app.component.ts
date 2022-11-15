b4c0aad0d6f34207e6aeae552298dc4333ad88da
SimoneCalfapietro <s.calfapietro@inquery.it>
2022-11-15 11:02:59 +0100

    correzioni arrotondamento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ShopItem } from './model/shop-item.model';
 import { enumSelector, Categories } from './model/category.enum';
 import { Cart } from './model/cart.model';
+import { formatNumber } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -32,59 +33,53 @@ export class AppComponent implements OnInit {
     let total = 0;
     let totalTax = 0;
     this.cart = new Cart();
-    debugger;
+    // debugger;
     this.items.forEach((item) => {
-      if (item.amount > 1) item.price = item.price * item.amount;
-      if (
-        item.category != Categories.meds &&
-        item.category != Categories.food &&
-        item.category != Categories.book &&
-        !item.imported
-      ) {
-        let rate = this.customRound(
-          Math.round(((item.price * this.tax) / 100 + Number.EPSILON) * 100) /
-            100
-        );
-        totalTax += rate;
-        item.price = item.price + rate;
-      } else if (
-        item.category != Categories.meds &&
-        item.category != Categories.food &&
-        item.category != Categories.book &&
-        item.imported
-      ) {
-        let rate = this.customRound(
-          Math.round(
-            ((item.price * (this.impTax + this.tax)) / 100 + Number.EPSILON) *
-              100
-          ) / 100
-        );
-        totalTax += rate;
-        item.price = item.price + rate;
-      } else if (
-        (item.category == Categories.meds ||
-        item.category == Categories.food ||
-        item.category == Categories.book) && item.imported
-      ) {
-        let rate = this.customRound(
-          Math.round(
-            ((item.price * this.impTax) / 100 + Number.EPSILON) * 100
-          ) / 100
-        );
-        totalTax += rate;
-        item.price = item.price + rate;
-      }
-      total += item.price;
-      this.cart.total = total;
-      this.cart.payedTax = totalTax;
+      let singlePrice = item.price;
+      for(let i = 0; i < item.amount; i++ ){
+        if (
+          item.category != Categories.meds &&
+          item.category != Categories.food &&
+          item.category != Categories.book &&
+          !item.imported
+        ) {
+          let rate = this.customRound((singlePrice * this.tax)/100);
+          totalTax += rate;
+          totalTax = Math.round((totalTax + Number.EPSILON)*100)/100;
+          item.price = Math.round(((singlePrice + rate)+Number.EPSILON)*100)/100;
+        } else if (
+          item.category != Categories.meds &&
+          item.category != Categories.food &&
+          item.category != Categories.book &&
+          item.imported
+        ) {
+          let rate = this.customRound((item.price * (this.impTax + this.tax)) / 100);
+          totalTax += rate;
+          totalTax = Math.round((totalTax + Number.EPSILON)*100)/100;
+          item.price = Math.round(((singlePrice + rate)+Number.EPSILON)*100)/100;
+        } else if (
+          (item.category == Categories.meds ||
+          item.category == Categories.food ||
+          item.category == Categories.book) && item.imported
+        ) {
+          let rate = this.customRound((singlePrice * this.impTax) / 100);
+          totalTax += rate;
+          totalTax = Math.round((totalTax + Number.EPSILON)*100)/100;
+          item.price = Math.round(((singlePrice + rate)+Number.EPSILON)*100)/100;
+        }
+        total += item.price;      
+      }      
       this.cart.items.push(item);
     });
+    this.cart.total = total;
+    this.cart.payedTax = totalTax;
     this.item = new ShopItem();
     this.items = [];
     console.log(this.cart);
   }
 
   customRound(num: number) {
-    return Math.round(num * 20) / 20.0;
+    // debugger;
+    return Math.ceil(num * 20) / 20.0;
   }
 }
